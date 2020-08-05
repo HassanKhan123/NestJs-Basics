a3166a98a02b51af232b1d382c48c715ebaf5268
HassanKhan123 <hk2602809@gmail.com>
2020-08-05 06:29:31 +0500

    get product by id api added

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,5 @@
 import { ProductsService } from './products.service';
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -21,5 +21,10 @@ export class ProductsController {
   @Get()
   getAllProducts(){
     return this.productsService.getProducts()
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId:string){
+    return this.productsService.getProduct(prodId)
   }
 }
